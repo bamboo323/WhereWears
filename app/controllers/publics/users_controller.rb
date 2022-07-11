a5b7c898e9d6a53d3a5006390043f0d26e7b7dbd
bamboo323 <bam9823@gmail.com>
2022-07-11 06:33:37 +0000

    Add フォロー/フォロワー機能

--- app/controllers/publics/users_controller.rb
+++ app/controllers/publics/users_controller.rb
@@ -27,6 +27,16 @@ class Publics::UsersController < ApplicationController
     end
   end
 
+  def followings
+    user = User.find(params[:id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:id])
+    @users = user.followers
+  end
+
   def exit
   end
 
